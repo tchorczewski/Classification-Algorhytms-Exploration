63ab6a8713aaccce346271bf688a321a2c5675bd
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2024-10-15 00:08:33 +0200

    Initial implementations, pending further work

--- main.py
+++ main.py
@@ -6,6 +6,50 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-cancer = pd.read_csv('data.csv')
+titanic_train = pd.read_csv('train.csv')
+titanic_test = pd.read_csv('test.csv')
+
+#Dropping columns that were deemed unimportant during visualization and data exploration
+titanic_train.drop(['Age','PassengerId', 'Ticket', 'Name','SibSp','Cabin'], axis=1, inplace=True)
+titanic_test.drop(['Age','PassengerId', 'Ticket', 'Name','SibSp','Cabin'], axis=1, inplace=True)
+#Dealing with null values
+titanic_train.dropna(subset=['Embarked'], axis=0, inplace=True)
+
+#Encoding categorical data
+label_encoder = LabelEncoder()
+titanic_train['Sex'] = label_encoder.fit_transform(titanic_train['Sex'])
+titanic_test['Sex'] = label_encoder.fit_transform(titanic_test['Sex'])
+
+titanic_train = pd.get_dummies(titanic_train,columns=['Embarked'], drop_first=True)
+titanic_test = pd.get_dummies(titanic_test,columns=['Embarked'], drop_first=True)
+print(titanic_train.info())
+#Preparing data for predictions
+x = titanic_train.drop('Survived', axis=1)
+y = titanic_train['Survived']
+
+x_train, x_validate,y_train,y_validate = train_test_split(x,y, random_state=42, test_size=0.25)
+
+logistic_regression = LogisticRegression()
+logistic_regression.fit(x_train,y_train)
+logistic_regression_results = logistic_regression.predict(x_validate)
+print('Logistic Regression')
+print(confusion_matrix(y_validate,logistic_regression_results))
+print(classification_report(y_validate,logistic_regression_results))
+
+random_forest = RandomForestClassifier()
+random_forest.fit(x_train,y_train)
+random_forest_results = random_forest.predict(x_validate)
+print('Random Forest')
+print(confusion_matrix(y_validate,random_forest_results))
+print(classification_report(y_validate,random_forest_results))
+
+svc = SVC()
+svc.fit(x_train,y_train)
+svc_results = svc.predict(x_validate)
+print('SVC')
+print(confusion_matrix(y_validate,svc_results))
+print(classification_report(y_validate,svc_results))
+
--- visualization.py
+++ visualization.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.impute import KNNImputer
-from sklearn.feature_selection import f_regression
 
 sns.set_theme()
 
@@ -35,16 +33,41 @@ print(titanic_train.loc[titanic_train['Embarked'].isna()])
 titanic_train.dropna(subset=['Embarked'], axis=0, inplace=True)
 print(titanic_train.isna().sum())
 
+#Dropping ID, Ticket and Name columns as they hold no information beneficial for the classification
+titanic_train.drop(['Ticket', 'Name', 'PassengerId'], inplace=True, axis=1)
+
 #Plotting the data
-sns.pairplot(data=titanic_train)
+sns.pairplot(data=titanic_train, corner=True)
 plt.show()
 
 #Plotting survival rates
-sns.displot(data=titanic_train, x='Sex', y='Pclass', hue='Survived')
-plt.title('')
-
-#Plotting Fare distribution
-sns.displot(data=titanic_train, x='Fare')
-plt.title('Fare distribution')
+sns.countplot(data=titanic_train, x='Survived')
+plt.title('Amount of Survivors and Not Survivors')
 plt.show()
 
+#Survivors distributed by gender
+sns.countplot(data=titanic_train, x='Sex', hue='Survived')
+plt.title('Survival rate vs Sex')
+plt.show()
+
+#Survival rate including passengers class
+sns.barplot(data=titanic_train, x='Sex', y='Survived', hue='Pclass')
+plt.title('Survival rate for passengers with different class')
+plt.show()
+
+#Survival rate per age group
+sns.swarmplot(data=titanic_train, x='Survived', y='Fare', hue='Survived')
+plt.title('Fare of People who did or did not survive')
+plt.show()
+
+#Survival rate per age
+sns.swarmplot(data=titanic_train, x='Survived', y='Age', hue='Sruvived')
+plt.title('Age distribution for survivors')
+plt.show()
+
+#Exploring the correlation
+sns.heatmap(data=titanic_train.corr(numeric_only=True), cmap='coolwarm', annot=True)
+plt.title('Correlation heatmap')
+plt.show()
+
+#After analyzing the plots and correlations the columns Age and SibSp will be dropped as their relation to survival of a person is minimal
