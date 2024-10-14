43350a960364d2fcf6b757c796ae4afaad01ab23
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2024-10-14 22:31:18 +0200

    Initial Commit

--- visualization.py
+++ visualization.py
@@ -2,14 +2,49 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.impute import KNNImputer
+from sklearn.feature_selection import f_regression
+
 sns.set_theme()
 
-spam = pd.read_csv('spam.csv')
+titanic_train = pd.read_csv('train.csv')
 
-print(spam.info())
-print(spam.describe())
-print(spam.head())
+#Exploration of train data
+print(titanic_train.info())
+print(titanic_train.describe())
+print(titanic_train.head())
 
 #Checking for missing data
-print(spam.isna().sum())
+print(titanic_train.isna().sum())
+
+#Visualization of missing data
+sns.heatmap(titanic_train.isna(), cmap='viridis')
+plt.show()
+
+#Dropping the Cabin column as there is a lot of data missing
+titanic_train.drop('Cabin', axis=1, inplace=True)
+
+#Filling missing age data using mean
+titanic_train['Age'] = titanic_train['Age'].fillna(titanic_train.groupby('Sex')['Age'].transform('mean'))
+print(titanic_train.isna().sum())
+
+#Checking 2 missing entries in Embarked column
+print(titanic_train.loc[titanic_train['Embarked'].isna()])
+
+#Removal of rows with missing entry in Embarked column
+titanic_train.dropna(subset=['Embarked'], axis=0, inplace=True)
+print(titanic_train.isna().sum())
+
+#Plotting the data
+sns.pairplot(data=titanic_train)
+plt.show()
+
+#Plotting survival rates
+sns.displot(data=titanic_train, x='Sex', y='Pclass', hue='Survived')
+plt.title('')
+
+#Plotting Fare distribution
+sns.displot(data=titanic_train, x='Fare')
+plt.title('Fare distribution')
+plt.show()
 
